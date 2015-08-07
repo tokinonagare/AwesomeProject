7bff773278fc379ebef4d93750ee3480984331cb
tokinonagare <timemahou@gmail.com>
2015-08-07 16:29:20 +0800

    Add ListView

--- index.ios.js
+++ index.ios.js
@@ -17,6 +17,7 @@ var MOCKED_MOVIES_DATA = [
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -25,7 +26,10 @@ var {
 var AwesomeProject = React.createClass({
   getInitialState: function() {
     return {
-      movies: null,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
     };
   },
   componentDidMount: function() {
@@ -36,19 +40,21 @@ var AwesomeProject = React.createClass({
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
-          movies: responseData.movies,
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
         });
       })
       .done();
   },
   render: function() {
-    if (!this.state.movies) {
+    if (!this.state.loaded) {
       return this.renderLoadingView();
     }
 
-    var movie = this.state.movies[0];
-    return this.renderMovie(movie);
-  },
+    return (
+      <ListView dataSource={this.state.dataSource} renderRow={this.renderMovie} style={styles.listView}/>
+      );
+   },
 
   renderLoadingView: function() {
     return (
@@ -59,6 +65,8 @@ var AwesomeProject = React.createClass({
       </View>
     );
   },
+
+
 
   renderMovie: function(movie) {
     return (
@@ -95,6 +103,10 @@ var styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
   // welcome: {
   //   fontSize: 20,
